3d6cc74dbcacd177fc695f5e7c300a77448e6e22
KiraneTMS <kirenaiasakura@gmail.com>
2023-10-09 00:52:58 +0700

    ?

--- app/src/main/java/com/example/adzan/MainActivity.kt
+++ app/src/main/java/com/example/adzan/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.adzan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adzan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Button untuk pindah ke ActivityKedua
+        binding.btnMoveToSecondActivity.setOnClickListener {
+            val dataToSend = "data Activity Pertama"
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("data", dataToSend)
+            startActivity(intent)
+        }
     }
 }
